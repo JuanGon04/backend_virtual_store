443716934830940c2543b3a91d7417f77843504c
Juan Gonzalez <juan.gonzalez09@uceva.edu.co>
2025-09-05 16:10:06 -0500

    Implementación de CORS, cookieParser, helmet para cabeceras de seguridad y validacion de entradas mediante validationsPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { envs } from '@common/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const origins =
+    envs.environment === 'prod' ? envs.originsProd : envs.originsDev;
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.use(helmet());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.enableCors({
+    origin: origins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  });
+  await app.listen(envs.port);
 }
 bootstrap();
